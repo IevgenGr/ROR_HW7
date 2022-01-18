7c74bfdd245dea86ceabd0f845784b99f54b5e8a
ievgen <jekripper@gmail.com>
2022-01-18 16:59:02 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/inspectionProfiles/Project_Default.xml

---
+++ app/models/comment.rb
@@ -0,0 +1,8 @@
+class Comment < ApplicationRecord
+  belongs_to :author
+  belongs_to :post
+  validates :body, presence: true
+  enum status: %i[unpublished published]
+  scope :published, -> { where("status = 1") }
+  scope :unpublished, -> { where("status = 0") }
+end
